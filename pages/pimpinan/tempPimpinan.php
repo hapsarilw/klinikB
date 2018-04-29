0aa60632585ed38004f670b729c6845b2e9fe40d
Andi Tangguh <anditangguh71@yahoo.com>
2018-04-29 18:58:38 +0700

    added new iud pasien, updated temppimpinan & logout

--- pages/pimpinan/tempPimpinan.php
+++ pages/pimpinan/tempPimpinan.php
@@ -1,28 +1,28 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <link rel="stylesheet" type="text/css" href="../../styles/doktor.css">
-    <script type="text/javascript" src="../../styles/doktor.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-<div id="pageBar">
-    <div class="sidenav">
-            <img id="fotoProfil" src="../../img/user.png" alt="fotoDokter">
-            <p id="namaD">Nama Pimpinan</p></a>
-            <p>ID Dokter</p></a>
-            <p>Pimpinan</p>
-            <p>Email</p>
-            <div id="logout">
-                <a href="../karyawan/karyawanlogin.html"><img style="width: 20px; height: 20px" src='../../img/logout.png'/><h4 id="keluar">keluar</h4></a>
+    <head>
+        <link rel="stylesheet" type="text/css" href="../../styles/doktor.css">
+        <script type="text/javascript" src="../../styles/doktor.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <div id="pageBar">
+            <div class="sidenav">
+                <img id="fotoProfil" src="../../img/user.png" alt="fotoDokter">
+                <p id="namaD">Nama Pimpinan</p>
+                <p>ID Dokter</p>
+                <p>Pimpinan</p>
+                <p>Email</p>
+                <div id="logout">
+                    <form action="../logout.php" method="post">
+                        <a name="submit">
+                            <button type="submit" name="submit">
+                                <img style="width: 20px; height: 20px" src='../../img/logout.png'/>Logout
+                            </button>
+                        </a>
+                    </form>
+                </div>
             </div>
-    </div>
-</div>
-
-
-
-<div>
-
-</div>
-</body>
+        </div>
+    </body>
 </html>
