c37b7a389cf152fc385929ba5bfad440fc85a6ab
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2018-05-02 09:40:07 +0700

    edit menu page dokter

--- pages/dokter/daftarpenanganan.php
+++ pages/dokter/daftarpenanganan.php
@@ -8,9 +8,9 @@
     </head>
     <body>
         <div class="topnav">
-            <a class="active" href="daftarpenanganan.php">Daftar Penanganan</a>
-            <a href="../viewData/dataJadwalPraktek.php">Atur Jadwal</a>
-            <a href="daftarcatatanpasien.php">Daftar Catatan Pasien</a>
+            <a class="active" href="daftarpenanganan.php?id_karyawan=<?php echo $_SESSION['idK']?>">Daftar Penanganan</a>
+            <a href="../viewData/dataJadwalPraktek.php?id_karyawan=<?php echo $_SESSION['idK']?>">Atur Jadwal</a>
+            <a href="daftarcatatanpasien.php?id_karyawan=<?php echo $_SESSION['idK']?>">Daftar Catatan Pasien</a>
         </div>
         <div class="content" >
             <h1>Cari Data Pasien</h1>
