00ecba4190607db826536ed44d8f27abcc9a17ed
Andi Tangguh <anditangguh71@yahoo.com>
2018-05-03 10:46:34 +0700

    finalize everything

--- FormHandling/fhDokter.php
+++ FormHandling/fhDokter.php
@@ -2,33 +2,27 @@
 // Load file koneksi.php
 include "../dbConnect.php";
 
-$id_k = $_GET['id_karyawan'];
+//$id_k = $_GET['id_karyawan'];
 
 $namaK = $_POST['nama_karyawan'];
 $email = $_POST['email'];
 $password = $_POST['password'];
-$namaSp = $_POST['nama_spesialisasi'];
+$namaSp = $_POST['jenisSp'];
+$idSp = $_POST['id_spesialisasi'];
 $jabatan = 'Dokter';
-$foto = $_FILES['foto']['name'];
-$tmp = $_FILES['foto']['tmp_name'];
 
-$fotobar = date('dmYHis').$foto;
+$query = "INSERT INTO karyawan(nama_karyawan, email, password, id_spesialisasi, jabatan) 
+              VALUES('".$namaK."', '".$email."', '".$password."', '".$idSp."','".$jabatan."')";
+$sql = mysqli_query($conn, $query) or die(mysqli_error($conn));
 
-$path = "C:/xampp/htdocs/GitHub/klinikB/pages/viewData/imgDokter/img".$fotobar;
 
-if(move_uploaded_file($tmp, $path)){
-    $query = "INSERT INTO karyawan(nama_karyawan, email, password, nama_spesialisasi, jabatan, foto) 
-              VALUES('".$namaK."', '".$email."', '".$password."', '".$namaSp."','".$jabatan."', '".$fotobaru."')";
-    $sql = mysqli_query($conn, $query) or die(mysqli_error($conn));
-
-    if($sql){ 
-        header("location: ../pages/viewData/dataDokter.php");
-    }else{
-        echo "Maaf, Terjadi kesalahan saat mencoba untuk menyimpan data ke database.";
-        echo "<br><a href='fhDokter.php'>Kembali Ke Form</a>";
-    }
+if($sql){ 
+//    $query2 = "INSERT INTO viewspesialisasidokter(nama_karyawan, email, password, id_spesialisasi, jenis_spesialisasi) 
+//              VALUES('".$namaK."', '".$email."', '".$password."', '".$idSp."','".$namaSp."')";
+//    $sql2 = mysqli_query($conn, $query2) or die(mysqli_error($conn));
+    header("location: ../pages/viewData/dataDokter.php");
 }else{
-    echo "Maaf, Gambar gagal untuk diupload.";
+    echo "Maaf, Terjadi kesalahan saat mencoba untuk menyimpan data ke database.";
     echo "<br><a href='fhDokter.php'>Kembali Ke Form</a>";
 }
 ?>
--- pages/pimpinan/addDokter.php
+++ pages/pimpinan/addDokter.php
@@ -16,7 +16,7 @@
         </div>
         <div class="content">
             <h1>Tambah Data Dokter</h1>
-            <form method="post" action="../../FormHandling/fhDokter.php=" enctype="multipart/form-data">
+            <form method="post" action="../../FormHandling/fhDokter.php" enctype="multipart/form-data">
                 <table cellpadding="8">
                     <tr>
                         <td>Nama Karyawan</td>
@@ -39,10 +39,12 @@
                             $sql = mysqli_query($conn, $query);
                             while($data = mysqli_fetch_array($sql)) {
                                 $jenisSp = $data['jenis_spesialisasi'];
-                                echo "<input type='radio' name='".$jenisSp."' value='".$jenisSp."'>".$jenisSp."<br>";
+                                $id_spesialisasi = $data['id_spesialisasi'];
+                                echo "<input type='radio' name='jenisSp' value='".$jenisSp."'>".$jenisSp."<br>";
                             }
                             ?>
                         </td>
+                        <input type="text" name="id_spesialisasi" style="display:none" value="<?php echo $id_spesialisasi; ?>">
                     </tr>
 
 <!--
